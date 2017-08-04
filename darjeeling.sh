2e6a9c155d6d3a2103809941ed4b9cf23f6e38a3
Shintaro Hori <hori@jsk.imi.i.u-tokyo.ac.jp>
2017-08-04 13:42:23 +0900

    use file path

--- darjeeling.sh
+++ darjeeling.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 slink=$0 #シンボリックリンクのパスを取得
-file=`readlink -f $0` #リンク先のファイルのパスを取得
+file=`readlink -f ${slink}` #リンク先のファイルのパスを取得
 dir=`dirname ${file}` #ファイルのパスからディレクトリのパスに
 prob="${dir}/proberbs.txt" #格言集のパス
 pekoe=$(cat ${prob} | shuf -n 1) #shufでランダムに一行読み出し
